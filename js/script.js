bfa2c7a180563e5414e9e03bd6e649d659bcca66
Nikonorov Gleb <g.s.nikonorov@gmail.com>
2021-09-27 22:29:07 +0300

    arzamas>cyber: fixed some bugs

--- js/script.js
+++ js/script.js
@@ -5,11 +5,15 @@ $(document).ready(function () {
   const topOfSidebarMobile = $(sidebar_mobile).offset().top;
   const sections = $('section');
   const sidebar_height = sidebar.outerHeight();
+  const sidebar_mobile_height = sidebar_mobile.outerHeight();
   const mobileSize = 667;
   $(window).scroll(function () {
 
-    var cur_pos = $(this).scrollTop(),
-    sections = $('.special-item__article-box');
+  var cur_pos = $(this).scrollTop(),
+  sections = $('.special-item__article-box'),
+  pageWidth = document.documentElement.clientWidth,
+  scrollTop = $(this).scrollTop();
+
     sections.each(function() {
       var top = $(this).offset().top - sidebar_height,
           bottom = top + $(this).outerHeight();
@@ -24,38 +28,38 @@ $(document).ready(function () {
       }
     });
 
-    sidebar.find('.sidebar-item').unbind().click(function () {
-      var id = $(this).find('a').attr('href'),
-          top = $(id).offset().top - sidebar_height;
-        if (id.indexOf('#') != -1) {
-          $('body,html').animate({ scrollTop: top }, 500);
-          if ($(".special-item__sidebar-mobile").is(':visible')) { //если мобильная версия
-              // то при переходе по ссылке закрываем меню
-              $('.special-item__sidebar').toggleClass('special-item__sidebar-mobile_visible');
-              $('.special-item__top-popup').toggleClass('special-item__top-popup_opened');
-              $('.special-item__top-popup > img').toggle();
-          }
-        }
-    });
-
-    $(window).scroll(function () {
-        var pageWidth = document.documentElement.clientWidth;
-        var scrollTop = $(this).scrollTop();
-        if(pageWidth <= mobileSize){//mobile
-          //через position:sticky
-        }else{//desktop
-          if (scrollTop  >= topOfSidebar - sidebar.position().top ) { // Если прокрутили больше, чем расстояние до блока, то приклеиваем его
-              $(sidebar).addClass("special-item__sidebar_visible");
-          } else {
-              $(sidebar).removeClass("special-item__sidebar_visible");
-          };
-        }
-    });
+    var pageWidth = document.documentElement.clientWidth;
+    var scrollTop = $(this).scrollTop();
+    if(pageWidth <= mobileSize){//mobile
+      //через position:sticky
+    }else{//desktop
+      if (scrollTop  >= topOfSidebar - sidebar.position().top ) { // Если прокрутили больше, чем расстояние до блока, то приклеиваем его
+          $(sidebar).addClass("special-item__sidebar_visible");
+      } else {
+          $(sidebar).removeClass("special-item__sidebar_visible");
+      };
+    }
   });
 
-    $('a>.special-item__top-popup').click(function(){
-      $('.special-item__sidebar').toggleClass('special-item__sidebar-mobile_visible');
-      $('.special-item__top-popup').toggleClass('special-item__top-popup_opened');
-      $('.special-item__top-popup > img').toggle();
-    });
+  sidebar.find('.sidebar-item').unbind().click(function () {
+    var id = $(this).find('a').attr('href'),
+        top = $(id).offset().top;
+      if (id.indexOf('#') != -1) {
+        $('body,html').animate({ scrollTop: top }, 500);
+        if ($(".special-item__sidebar-mobile").is(':visible')) { //если мобильная версия
+            // то при переходе по ссылке закрываем меню
+            openClosePopUp();
+      }
+    }
+  });
+
+  $('a>.special-item__top-popup').click(function(){
+    openClosePopUp();
+  });
+
+  function openClosePopUp(){
+    sidebar.toggleClass('special-item__sidebar-mobile_visible');
+    $('.special-item__top-popup').toggleClass('special-item__top-popup_opened');
+    $('.special-item__top-popup > img').toggle();
+  }
 });
