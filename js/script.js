e6a0abae93437846718c584737e06a5833b3df8e
mazeoff <nikonorov@rptp.org>
2021-09-28 14:31:33 +0300

    arzamas>cyber: changed the behavior of the appearance of the menu on the pc version

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 $(document).ready(function () {
   const sidebar = $(".special-item__sidebar");
   const sidebar_mobile = $(".special-item__sidebar-mobile");
+  const topPopUP = $(".special-item__top-popup");
   const topOfSidebar = $(sidebar).offset().top;
   const topOfSidebarMobile = $(sidebar_mobile).offset().top;
   const sections = $('section');
-  const sidebar_height = sidebar.outerHeight();
-  const sidebar_mobile_height = sidebar_mobile.outerHeight();
   const mobileSize = 667;
 
   $(window).scroll(function () {
@@ -16,7 +15,7 @@ $(document).ready(function () {
 
     sections.each(function() {
       var top = $(this).offset().top,
-          bottom = top - sidebar_height + $(this).outerHeight();
+          bottom = top - sidebar.outerHeight() - topPopUP.outerHeight() + $(this).outerHeight();
 
       if (cur_pos >= top && cur_pos <= bottom) {
 
@@ -25,18 +24,19 @@ $(document).ready(function () {
 
       $(this).addClass('special-item__article-box_active');
       sidebar.find('a[href="#'+$(this).attr('id')+'"]').addClass('sidebar-item_active');
-      }
+    }
     });
 
     var pageWidth = document.documentElement.clientWidth;
+    var article_list_bottom = $(".special-item__article-list").offset().top + $(".special-item__article-list").height();
     if(pageWidth <= mobileSize){//mobile
       //через position:sticky
     }else{//desktop
-      if (cur_pos  >= topOfSidebar - sidebar.position().top ) { // Если прокрутили больше, чем расстояние до блока, то приклеиваем его
+      if ((cur_pos  >= topOfSidebar - sidebar.position().top) && (cur_pos  <= (article_list_bottom - sidebar.position().top) ) ) { // Если прокрутили больше, чем расстояние до блока, то приклеиваем его
           $(sidebar).addClass("special-item__sidebar_visible");
       } else {
           $(sidebar).removeClass("special-item__sidebar_visible");
-      };
+      }
     }
   });
 
@@ -44,10 +44,10 @@ $(document).ready(function () {
     var id = $(this).find('a').attr('href'),
         top = $(id).offset().top;
       if (id.indexOf('#') != -1) {
-        if ($(".special-item__sidebar-mobile").is(':visible')) { //если мобильная версия
+        if (sidebar_mobile.is(':visible')) { //если мобильная версия
           // то при переходе по ссылке закрываем меню
           openClosePopUp();
-          $('body,html').animate({ scrollTop: top - sidebar_mobile_height - sidebar_height }, 500);
+          $('body,html').animate({ scrollTop: top - sidebar.outerHeight() - topPopUP.outerHeight() }, 500);
         }else{
           $('body,html').animate({ scrollTop: top }, 500);
         }
